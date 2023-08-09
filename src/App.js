92ba1217713eefcb43a66d82b91eebbcaf32afc5
unknown <anaester.gonzalez@live.com>
2023-08-09 19:40:02 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+import imageRickMorty from "./img/rick-morty.png";
+import "./App.css";
+import{useState} from "react";
+import Characters from "./components/Characters";
+
+function App() {
+  const [characters, setCharacters]= useState(null);
+  const reqApi= async () =>{
+  const api= await fetch("https://rickandmortyapi.com/api/character");
+  const CharacterApi= await api.json();
+
+    setCharacters(CharacterApi.results);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title">Rick y morty</h1>
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters} />
+
+
+        ) : (
+          <>
+          <img src={imageRickMorty} alt="Rick y morty" className="img-home"/>
+          <button onClick={reqApi} className="btn-search">
+            Buscar personajes</button>
+          </>
+
+        )}
+
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
